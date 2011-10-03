da14f5de179bb0f12ad1069104405316c9c7b893
jianhuashao <jianhua.shao1986@gmail.com>
2011-10-03 12:55:55 +0100

    init djangoappengine

---
+++ jwl/jwl/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
+    url(r'^admin/', include(admin.site.urls)),
+)
